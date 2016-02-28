5391b47b0d66aaa918539165395d6f2d8c16e32e
Debosmit Ray <dray92@uw.edu>
2016-02-27 18:21:19 -0800

    added ideation

--- Peculiar_Balance.java
+++ Peculiar_Balance.java
@@ -8,6 +8,25 @@ public class Peculiar_Balance {
 		
 		int nextHigherPow = getNextHigherPower(x);
 		
+		/** 
+		 * Ideation:
+		 * Say, x is 25. This means I have 25 on the left
+		 * side. The nextHigherPower of 3 from 25 is 27.
+		 * Hence, out result set will have a size of 1 + log(27)[base 3],
+		 * which is 1 + 3 = 4, since the 3^3 = 27.
+		 * The result set will look like [" ", " ", " ", " "], where the first
+		 * index shows position of 1(3^0), second shows position of 3(3^1),
+		 * third shows position of 9(3^2), and fourth shows position of 27(3^3).
+		 * 
+		 * Current difference in sides = +25	(+ => left is more) (- => right is more)
+		 * First weight being considered is 27. Now, will adding 27 to the left decrease
+		 * the absolute value of the difference? (+25 + 27 < |+25|)? No. Will adding 27 to 
+		 * the right decrease the absolute value of the difference? (+25 - 27 < |+25|). Yes.
+		 * Lets do it!
+		 * []
+		 */
+		
+		
 		String[] result = new String[nextHigherPow+1];
 		// this loop ensures that the same weight isn't encountered more than once
 		for(int i = nextHigherPow ; i >= 0 ; i--) {
@@ -62,5 +81,6 @@ public class Peculiar_Balance {
 	
 	public static void main(String[] args) {
 		System.out.println(Arrays.toString(answer(999999999)));
+		System.out.println(Arrays.toString(answer(25)));
 	}
 }
