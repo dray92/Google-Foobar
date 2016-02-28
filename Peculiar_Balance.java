37ac90c124e137af9e887f8b5221d35c92966e53
Debosmit Ray <dray92@uw.edu>
2016-02-27 17:05:14 -0800

    answer2 is the new one. test params -> 500000 iterations. n = 999999999 answer  -> 1420 milliseconds answer2 ->  102 milliseconds
    benchmarking code included. will be removed in future commit

--- Peculiar_Balance.java
+++ Peculiar_Balance.java
@@ -93,9 +93,108 @@ public class Peculiar_Balance {
 		return retVal;
 	}
 	
-	public static void main(String[] args) {
+	public static String[] answer2(int x) {
 		
-		String[] ans = answer(1000000000 - 1);
+		getPow3Array();
+		int nextHigherPow = getNextHigherPower(x);
+		
+		String[] result = new String[nextHigherPow+1];
+		// this loop ensures that the same weight isn't encountered more than once
+		for(int i = nextHigherPow ; i >= 0 ; i--) {
+			int weight = pow3[i];
+			// now, where to add to add this weight
+			// our goal is that after adding this weight, 
+			// the net difference between the left and
+			// right sides must go down
+			
+			// add to left side
+			if( Math.abs(x+weight) < Math.abs(x) ) {
+				x += weight;
+				result[i] = "L";
+			} 
+			
+			// add to right
+			else if( Math.abs(x-weight) < Math.abs(x) ) {
+				x -= weight;
+				result[i] = "R";
+			} 
+			
+			// if weight is not affecting anything, don't need to add
+			else {
+				result[i] = "-";
+			}
+		}
+			
+		return result;
+	}
+	
+	private static int getNextHigherPower(int x) {
+		for(int i = pow3.length-1 ; i>= 0 ; i--) {
+			if(pow3[i] == x)
+				return i;
+			if(pow3[i] < x)
+				return i+1;
+		}
+		return pow3.length;
+	}
+	
+	private static int[] pow3;
+	private static void getPow3Array() {
+		if(pow3 == null) {
+			// log(1000000000)/log(3) = 18.8631294686
+			pow3 = new int[2 + (int)(Math.log10(1000000000)/Math.log10(3))];
+			pow3[0] = 1;
+			for(int i = 1 ; i < pow3.length ; i++)
+				pow3[i] = 3 * pow3[i-1];
+		}
+	}
+	
+	public static void main(String[] args) {
+		int NUM_ITERS = 500000;
+		String[] ans = null;
+		
+		long start = System.currentTimeMillis();
+		
+//		while(NUM_ITERS-- > 0) {
+//			ans = answer(1000000000 - 1);
+//		}
+		System.out.println( "Time: " + (System.currentTimeMillis() - start) + " msecs");
+		
 		System.out.println(Arrays.toString(ans));
+		
+		int left = 0;
+		int right = 0;
+//		for(int i = 0 ; i < ans.length ; i++) {
+//			if(ans[i].compareTo("L") == 0)
+//				left += Math.pow(3, i);
+//			else if(ans[i].compareTo("R") == 0)
+//				right += Math.pow(3, i);
+//		}
+		System.out.println("left =  " + left);
+		System.out.println("right = " + right);
+		
+		
+		ans = null;
+		start = System.currentTimeMillis();
+		
+		while(NUM_ITERS-- > 0) {
+			ans = answer2(1000000000 - 1);
+		}
+		System.out.println( "Time: " + (System.currentTimeMillis() - start) + " msecs");
+		
+		System.out.println(Arrays.toString(ans));
+		
+		
+		
+		left = 0;
+		right = 0;
+		for(int i = 0 ; i < ans.length ; i++) {
+			if(ans[i].compareTo("L") == 0)
+				left += Math.pow(3, i);
+			else if(ans[i].compareTo("R") == 0)
+				right += Math.pow(3, i);
+		}
+		System.out.println("left =  " + left);
+		System.out.println("right = " + right);
 	}
 }
