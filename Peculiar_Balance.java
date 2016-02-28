dcb34f2eb15d1016419daf137c1e46425218ead2
Debosmit Ray <dray92@uw.edu>
2016-02-27 18:23:38 -0800

    added complete ideation

--- Peculiar_Balance.java
+++ Peculiar_Balance.java
@@ -17,13 +17,37 @@ public class Peculiar_Balance {
 		 * The result set will look like [" ", " ", " ", " "], where the first
 		 * index shows position of 1(3^0), second shows position of 3(3^1),
 		 * third shows position of 9(3^2), and fourth shows position of 27(3^3).
+		 * Our result set has a 0-based index.
 		 * 
+		 * Current power being considered is 3.
 		 * Current difference in sides = +25	(+ => left is more) (- => right is more)
 		 * First weight being considered is 27. Now, will adding 27 to the left decrease
-		 * the absolute value of the difference? (+25 + 27 < |+25|)? No. Will adding 27 to 
-		 * the right decrease the absolute value of the difference? (+25 - 27 < |+25|). Yes.
+		 * the absolute value of the difference? (|+25 + 27| < |+25|)? No. Will adding 27 to 
+		 * the right decrease the absolute value of the difference? (|+25 - 27| < |+25|). Yes.
 		 * Lets do it!
-		 * []
+		 * [" ", " ", " ", "R"]
+		 * 
+		 * Then, consider the next power. Power 2.
+		 * Current difference in sides = -2.
+		 * Weight being considered is 9. Now, will adding 9 to the left decrease
+		 * the absolute value of the difference? (|-2 + 9| < |-2|)? No. Will adding 9 to 
+		 * the right decrease the absolute value of the difference? (|+25 - 27| < |+2|). No.
+		 * Then, the weight of 9 is useless to our purposes.
+		 * [" ", " ", "-", "R"]
+		 * 
+		 * Then, consider the next power. Power 1.
+		 * Current difference in sides = -2.
+		 * Weight being considered is 3. Now, will adding 3 to the left decrease
+		 * the absolute value of the difference? (|-2 + 3| < |-2|)? Yes. Lets do it!
+		 * [" ", "L", "-", "R"]
+		 * 
+		 * Then, consider the next power. Power 0.
+		 * Current difference in sides = +1.
+		 * Weight being considered is 1. Now, will adding 1 to the left decrease
+		 * the absolute value of the difference? (|+1 + 1| < |+1|)? No. Will adding 1 to 
+		 * the right decrease the absolute value of the difference? (|+1 - 1| < |+1|). Yes.
+		 * Lets do it!
+		 * ["R", "L", "-", "R"]
 		 */
 		
 		
