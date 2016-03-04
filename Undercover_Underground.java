4309ac63d2bbb6f749e1a893fbb9dd1850626e4d
Debosmit Ray <dray92@uw.edu>
2016-03-04 01:39:01 -0800

    challenge completed

--- Undercover_Underground.java
+++ Undercover_Underground.java
@@ -109,12 +109,37 @@ public class Undercover_Underground {
 		return fact;
 	}
 
+	/**
+	 * Idea:
+	 * An edge is defined to be a connection between two nodes
+	 * for our purposes. For reference, we have N nodes and K
+	 * edges in our desired graph(s).
+	 * 
+	 * With N nodes, we can have {N}C{2} graphs (that many possible edges).
+	 * For our K, this set is reduced to { {N}C{2} }C{K}
+	 * 
+	 * We are only interested in the simply connected graphs. Lets call this
+	 * set of graphs C[N,K].
+	 * 
+	 * To transform a graph c in C[N,K], to a graph in C[N,K+1], we take
+	 * 2 unconnected nodes u,v: u,v ∈ c, and connect them.
+	 * 
+	 * To transform a graph c in C[N,K] to a graph in C[N+1,K], it is a 
+	 * little more work. We take 2 nodes in c, that are either directly
+	 * or indirectly connected. Either way, in order to keep the number of edges
+	 * constant, there would have to 2 at least 2 paths connecting the 2 nodes.
+	 * We would get rid of one of these edge connections and add our new node
+	 * to either of the 2 nodes to create 2 separate graphs.
+	 * 
+	 * This is the general concept. To implement, I decided to start with our max
+	 * possible number of graphs and remove all graphs that have either unconnected
+	 * nodes, self loops, incomplete graphs and complex graphs (i think that is all)
+	 */
+	
 	static Map<List<Integer>, String> resultMap = new HashMap<List<Integer>, String>();
-	// N -> number of nodes
-	// K -> number of edges
-	// N will be at least 2 and at most 20.
-	// K will be at least one less than n and at most (n * (n - 1)) / 2
+	// loose reference: http://math.stackexchange.com/questions/689526/how-many-connected-graphs-over-v-vertices-and-e-edges
 	public static String answer(int N, int K) {
+		
 		/* for the case where K < N-1 */
 		if(K < N-1)
 			return BigInteger.ZERO.toString();
@@ -149,67 +174,17 @@ public class Undercover_Underground {
 		if(K > maxEdges)
 			return BigInteger.ZERO.toString();
 		
-		BigInteger count = BigInteger.ZERO;
-		
-		for(int k = 1; k <= N-1 ; k++) {
-			BigInteger combinations = nChooseR(N-1, k);
-			combinations = combinations.multiply(new BigInteger(answer(N-1, K-k)));
-			count = count.add(combinations);
-		}
-		
-		// unmodifiable so key cannot change hash code
-		resultMap.put(Collections.unmodifiableList(Arrays.asList(N, K)), count.toString());
-		
-		return count.toString();
-	}
-	
-	static Map<List<Integer>, String> resultMap2 = new HashMap<List<Integer>, String>();
-	// reference: http://math.stackexchange.com/questions/689526/how-many-connected-graphs-over-v-vertices-and-e-edges
-	public static String answer2(int N, int K) {
-		/* for the case where K < N-1 */
-		if(K < N-1)
-			return BigInteger.ZERO.toString();
-		
-		/* for the case where K = N-1 */
-		// Cayley's formula applies [https://en.wikipedia.org/wiki/Cayley's_formula].
-		// number of trees on n labeled vertices is n^{n-2}.
-		if(K == N-1)
-			return BigInteger.valueOf((long)Math.pow(N, N-2)).toString();
-		
-		/* for the case where K > N-1 */
-		// check if key is present in the map
-		List<Integer> tuple = Arrays.asList(N, K);
-		if( resultMap2.containsKey(tuple) )
-			return resultMap2.get(tuple);
-						
-		// maximum number of edges in a simply 
-		// connected undirected unweighted graph 
-		// with n nodes = |N| * |N-1| / 2
-		int maxEdges = N * (N-1) / 2;
-		
-		/* for the case where K = N(N-1)/2 */
-		// if K is the maximum possible 
-		// number of edges for the number of 
-		// nodes, then there is only one way is 
-		// to make a graph (connect each node
-		// to all other nodes)
-		if(K == maxEdges)
-			return BigInteger.ONE.toString();
-		
-		/* for the case where K > N(N-1)/2 */
-		if(K > maxEdges)
-			return BigInteger.ZERO.toString();
-		
 		// get the universal set
 		BigInteger allPossible = nChooseR(maxEdges, K);
 		
 		BigInteger repeats = BigInteger.ZERO;
 		// now, to remove duplicates, or incomplete graphs
-		// when can these cases occur?
+		// when can these cases occur? [note: 2 edges kept in hand]
 		for(int n = 0 ; n <= N-2 ; n++) {
 			
 			BigInteger choose_n_from_rem_nodes = nChooseR(N-1, n);
 			
+			// how many ways are there to keep 2 edges in hand?
 			// this is essentially choosing N-3-n nodes from N-1-n
 			// which is (N-1-n) choose (N-3-n)
 			// = {N-1-n}! / { {N-3-n}! {(N-1-n)-(N-3-n)}!
@@ -221,7 +196,7 @@ public class Undercover_Underground {
 			for(int k = 0 ; k <= K ; k++) {
 				BigInteger combinations = nChooseR(chooseN, k);
 				
-				BigInteger recurse = new BigInteger(answer2(n+1, K-k));
+				BigInteger recurse = new BigInteger(answer(n+1, K-k));
 				
 				repeatedEdges = repeatedEdges.add(combinations.multiply(recurse));
 			}
@@ -233,24 +208,17 @@ public class Undercover_Underground {
 		allPossible = allPossible.subtract(repeats);
 		
 		// add to cache
-		resultMap2.put(Collections.unmodifiableList(Arrays.asList(N, K)), allPossible.toString());
-		return resultMap2.get(tuple);
+		resultMap.put(Collections.unmodifiableList(Arrays.asList(N, K)), allPossible.toString());
+		return resultMap.get(tuple);
 	}
 	
 	public static void main(String[] args) {
-		int fail = 0;
-		int total = 0;
 		for(int n = 2 ; n <= 20 ; n++) {
 			for(int k = n-1 ; k <= n*(n-1)/2 ; k++) {
-				total++;
 				String ans = answer(n,k);
-				String ans2 = answer2(n,k);
-				if(ans.compareTo(ans2) != 0) {
-					fail++;
-					System.out.println("N = " + n + " , K = " + k + " , num = " + ans + " ||| " + ans2);
-				}
+				System.out.println("N = " + n + " , K = " + k + " , num = " + ans);
+				
 			}
 		}
-		System.out.println("Approach 1 fails " + ((100*fail)/total) + "% of the test");
 	}
 }
